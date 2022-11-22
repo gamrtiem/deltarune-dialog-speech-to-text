b306c00a216212d49b9f756efd4d5205770da65c
gamrtiem <89434652+gamrtiem@users.noreply.github.com>
2022-11-21 17:52:55 -0800

    whole lot of stuff like making it all work and icons

--- main.py
+++ main.py
@@ -3,19 +3,22 @@ import pyaudio
 import pygame
 pygame.init()
 
-x = 296
-y = 82
+x = 1334
+y = 376
 z = "hello wrold"
 
 pygame.display.set_mode((x,y))
 
 scrn = pygame.display.set_mode((x, y))
 	
+portrait = pygame.image.load("images\\ralsei_happy.png").convert()
+portrait = pygame.transform.scale(portrait, (262, 220))
+scrn.blit(portrait, (91, 85))
 
-imp = pygame.image.load("images\\dialog.png").convert()
-scrn.blit(imp, (0, 0))
+dialog = pygame.image.load("images\\dialog.png").convert()
+scrn.blit(dialog, (0, 0))
 
-font_1 = pygame.font.Font("font\\determinationsansweb-webfont.ttf", 20)
+font_1 = pygame.font.Font("font\\determinationMono.ttf", 59)
 
 
 model = Model(r"vosk-model-small-en-us-0.15")
@@ -34,17 +37,17 @@ running  = True
 while running:  
 
     text = font_1.render(z, True, (255, 255, 255))
-    scrn.blit(text, (0, 0))
+    scrn.blit(text, (337,64))
 
     pygame.display.update()
     data = stream.read(4096)
         
-    scrn.blit(imp, (0, 0))
-
+    scrn.blit(dialog, (0, 0))
+    scrn.blit(portrait, (74, 86))
     if recognizer.AcceptWaveform(data):
         text2 = recognizer.Result()
         print(f"' {text2[14:-3]} '")
-        z = f" {text2[14:-3]} "
+        z = "*" + f" {text2[14:-3]} "
 
 
     for event in pygame.event.get():  
