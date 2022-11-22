3c1202fc17073fe9b0e32f2d1bec36feb90fd92a
gamrtiem <89434652+gamrtiem@users.noreply.github.com>
2022-11-22 14:41:39 -0800

    makes it actually work no way !!!

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from vosk import Model, KaldiRecognizer
 import pyaudio
 import pygame
+import time
 pygame.init()
 
 x = 1334
 y = 376
-z = "hello wrold"
+z = "hellow rosld"
+linebreak = 0
+n = "* "
+b = ""
+a = ""
+k = ""
+change = ""
 
 pygame.display.set_mode((x,y))
 
 scrn = pygame.display.set_mode((x, y))
 	
-portrait = pygame.image.load("images\\ralsei_happy.png").convert()
+portrait = pygame.image.load("images\\ralsei_idle.png").convert()
 portrait = pygame.transform.scale(portrait, (262, 220))
 scrn.blit(portrait, (91, 85))
 
@@ -31,23 +38,66 @@ stream.start_stream()
     
 
 
+g = 0 
 
 pygame.display.flip()
 running  = True
 while running:  
-
-    text = font_1.render(z, True, (255, 255, 255))
-    scrn.blit(text, (337,64))
-
-    pygame.display.update()
-    data = stream.read(4096)
-        
-    scrn.blit(dialog, (0, 0))
-    scrn.blit(portrait, (74, 86))
+    data = stream.read(8192)
     if recognizer.AcceptWaveform(data):
         text2 = recognizer.Result()
         print(f"' {text2[14:-3]} '")
-        z = "*" + f" {text2[14:-3]} " 
+        z = "*" + f" {text2[14:-3]}" 
+        print(len(z) - 2)
+
+    def print_string(sentence):
+        count = 0
+
+        while count < len(sentence):
+           global k
+           k = k + sentence[count]
+           o = k
+           text = font_1.render(o, False, (255, 255, 255))
+           scrn.blit(text, (337,64))
+           print(k)
+           pygame.display.update()
+           count += 1
+           time.sleep(0.05)
+    
+
+
+    
+
+    g = 0
+    if g == 0:
+        
+        g = 1
+
+    scrn.blit(portrait, (74, 86))
+
+        
+
+  #  for i in range(0, len(z) - 2):
+  #      if i > 25 and not i > 50:
+  #          b[i - 24] = z[i]
+  #          if i > 50 and not i > 75:
+  #              a[i - 49] = z[i]
+  #              if i > 75 and not i > 100:
+  #                  z[i] = " "
+  #          z[i] = " "
+    if not change == z:
+        scrn.blit(dialog, (0, 0))
+        scrn.blit(portrait, (74, 86))
+        print_string(z)
+        print_string(b)
+        print_string(a)
+        change = z
+
+
+    
+
+    k = ""
+
 
 
     for event in pygame.event.get():  
